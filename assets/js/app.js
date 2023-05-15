b48f9280c56ccf8c281e48cf7735dd598904fad1
shafik-soweb <shafiksoweb191@gmail.com>
2023-05-15 23:22:03 +0600

    header and footer extracted

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,18 @@ fetch("header.html")
 fetch("footer.html")
   .then((response) => response.text())
   .then((data) => (document.querySelector("#footer").innerHTML = data));
+
+// Toggle navbar
+function toggle() {
+  const menu = document.querySelector("#menu");
+  menu.classList.toggle("hidden");
+}
+function goUp() {
+  const scrollTop = () => {
+    if (document.documentElement.scrollTop > 0) {
+      window.scrollBy(0, -50);
+      requestAnimationFrame(scrollTop);
+    }
+  };
+  scrollTop();
+}
