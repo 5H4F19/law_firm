1ba1b06fe69241ef6e53b917b062e0e1ed656338
shafik-soweb <shafiksoweb191@gmail.com>
2023-05-15 10:57:16 +0600

    new files added

---
+++ data/practices.js
@@ -0,0 +1,278 @@
+export const data = [
+  { type: "Anti-Bribery", img: "/assets/images/practices/image 1.svg" },
+  {
+    type: "Anti-trust and Competition Counselling",
+    img: "/assets/images/practices/image 2.svg",
+  },
+  { type: "Arbitration", img: "/assets/images/practices/image 3.svg" },
+  { type: "Asset Finance", img: "/assets/images/practices/image 4.svg" },
+  {
+    type: "Banking and Finance Regulation",
+    img: "/assets/images/practices/image 5.svg",
+  },
+  {
+    type: "Bankruptcy and Insolvency Disputes",
+    img: "/assets/images/practices/image 6.svg",
+  },
+  {
+    type: "Board Secretariat Functions and Directors’ responsibilities",
+    img: "/assets/images/practices/image 7.svg",
+  },
+  { type: "Business Services", img: "/assets/images/practices/image 8.svg" },
+  {
+    type: "Capital Market Licensing",
+    img: "/assets/images/practices/image 9.svg",
+  },
+  { type: "Cloud Computing", img: "/assets/images/practices/image 10.svg" },
+  {
+    type: "Commercial contracts",
+    img: "/assets/images/practices/image 11.svg",
+  },
+  { type: "Capital Markets", img: "/assets/images/practices/image 12.svg" },
+  {
+    type: "Competition and Antitrust Disputes",
+    img: "/assets/images/practices/image 13.svg",
+  },
+  { type: "Compliance", img: "/assets/images/practices/image 14.svg" },
+  { type: "Construction", img: "/assets/images/practices/image 15.svg" },
+  {
+    type: "Construction Claims and Disputes",
+    img: "/assets/images/practices/image 16.svg",
+  },
+  { type: "Corporate", img: "/assets/images/practices/image 17.svg" },
+  { type: "Corporate Finance", img: "/assets/images/practices/image 18.svg" },
+  { type: "Corporate Services", img: "/assets/images/practices/image 19.svg" },
+  {
+    type: "Corporate Structuring",
+    img: "/assets/images/practices/image 20.svg",
+  },
+  { type: "Cybersecurity", img: "/assets/images/practices/image 21.svg" },
+  { type: "Data Protection", img: "/assets/images/practices/image 22.svg" },
+  { type: "Debt Conversion", img: "/assets/images/practices/image 23.svg" },
+  {
+    type: "Derivatives and Structured Finance",
+    img: "/assets/images/practices/image 24.svg",
+  },
+  { type: "Dispute Resolution", img: "/assets/images/practices/image 25.svg" },
+  { type: "Employment Disputes", img: "/assets/images/practices/image 26.svg" },
+  { type: "Employment Advisory", img: "/assets/images/practices/image 27.svg" },
+  {
+    type: "Environmental, Health and Safety Regulatory Compliance",
+    img: "/assets/images/practices/image 28.svg",
+  },
+  {
+    type: "Environmental, Social, and Corporate Governance (ESG) disclosures",
+    img: "/assets/images/practices/image 29.svg",
+  },
+  { type: "Family businesses", img: "/assets/images/practices/image 30.svg" },
+  { type: "Finance", img: "/assets/images/practices/image 31.svg" },
+  {
+    type: "Financial Services and Securities Disputes",
+    img: "/assets/images/practices/image 32.svg",
+  },
+  { type: "Fund Finance", img: "/assets/images/practices/image 33.svg" },
+  {
+    type: "Funds & Asset Management",
+    img: "/assets/images/practices/image 34.svg",
+  },
+  {
+    type: "Fintech",
+    img: "/assets/images/practices/image 35.svg",
+  },
+  {
+    type: "General Litigation",
+    img: "/assets/images/practices/image 36.svg",
+  },
+  {
+    type: "IPOs",
+    img: "/assets/images/practices/image 37.svg",
+  },
+  {
+    type: "Insurance and Reinsurance",
+    img: "/assets/images/practices/image 38.svg",
+  },
+  {
+    type: "Government Procurement",
+    img: "/assets/images/practices/image 39.svg",
+  },
+  {
+    type: "Intellectual Property Disputes",
+    img: "/assets/images/practices/image 40.svg",
+  },
+  {
+    type: "Intellectual Property Rights",
+    img: "/assets/images/practices/image 41.svg",
+  },
+  {
+    type: "Investigations, Enforcement and Business Crime Disputes",
+    img: "/assets/images/practices/image 42.svg",
+  },
+  {
+    type: "Investment Funds",
+    img: "/assets/images/practices/image 43.svg",
+  },
+  {
+    type: "Investment Treaty Disputes",
+    img: "/assets/images/practices/image 44.svg",
+  },
+  {
+    type: "Islamic Project Finance",
+    img: "/assets/images/practices/image 45.svg",
+  },
+  {
+    type: "Islamic Derivatives and Structured Finance",
+    img: "/assets/images/practices/image 46.svg",
+  },
+  {
+    type: "Islamic Finance",
+    img: "/assets/images/practices/image 47.svg",
+  },
+  {
+    type: "Leveraged & Acquisition Finance",
+    img: "/assets/images/practices/image 48.svg",
+  },
+  {
+    type: "Joint ventures & Strategic alliances",
+    img: "/assets/images/practices/image 49.svg",
+  },
+  {
+    type: "Leasing",
+    img: "/assets/images/practices/image 50.svg",
+  },
+  {
+    type: "Legislation, Public Policy and Regulatory",
+    img: "/assets/images/practices/image 51.svg",
+  },
+  {
+    type: "Microfinance and Social Investment",
+    img: "/assets/images/practices/image 52.svg",
+  },
+  {
+    type: "Mediation and Amicable Settlement",
+    img: "/assets/images/practices/image 53.svg",
+  },
+  {
+    type: "Merger Control Filings",
+    img: "/assets/images/practices/image 54.svg",
+  },
+  {
+    type: "Mergers & Acquisitions",
+    img: "/assets/images/practices/image 55.svg",
+  },
+  {
+    type: "Private Sale Transactions",
+    img: "/assets/images/practices/image 56.svg",
+  },
+  {
+    type: "Procurement",
+    img: "/assets/images/practices/image 57.svg",
+  },
+  {
+    type: "Project Development",
+    img: "/assets/images/practices/image 58.svg",
+  },
+  {
+    type: "Private Equity",
+    img: "/assets/images/practices/image 59.svg",
+  },
+  {
+    type: "Private Equity",
+    img: "/assets/images/practices/image 59.svg",
+  },
+  {
+    type: "Projects",
+    img: "/assets/images/practices/image 60.svg",
+  },
+  {
+    type: "Public Companies Capital Decrease",
+    img: "/assets/images/practices/image 61.svg",
+  },
+  {
+    type: "Project Finance",
+    img: "/assets/images/practices/image 62.svg",
+  },
+  {
+    type: "Project-Related Dispute Settlements",
+    img: "/assets/images/practices/image 63.svg",
+  },
+  {
+    type: "Public and Administrative Law Disputes",
+    img: "/assets/images/practices/image 64.svg",
+  },
+  {
+    type: "Public-Private Partnerships (PPP)",
+    img: "/assets/images/practices/image 65.svg",
+  },
+  {
+    type: "Public Companies Corporate Governance and Disclosers",
+    img: "/assets/images/practices/image 66.svg",
+  },
+  {
+    type: "Public M&A",
+    img: "/assets/images/practices/image 67.svg",
+  },
+  {
+    type: "Real Estate Disputes",
+    img: "/assets/images/practices/image 68.svg",
+  },
+  {
+    type: "Real Estate Finance",
+    img: "/assets/images/practices/image 69.svg",
+  },
+  {
+    type: "Real Estate Investment Funds and REITs",
+    img: "/assets/images/practices/image 70.svg",
+  },
+  {
+    type: "Real Estate Development",
+    img: "/assets/images/practices/image 71.svg",
+  },
+  {
+    type: "Restructuring and Insolvency",
+    img: "/assets/images/practices/image 72.svg",
+  },
+  {
+    type: "Restructuring and reorganization",
+    img: "/assets/images/practices/image 73.svg",
+  },
+  {
+    type: "Rights Issue",
+    img: "/assets/images/practices/image 74.svg",
+  },
+  {
+    type: "Real Estate Permits and Authorizations",
+    img: "/assets/images/practices/image 75.svg",
+  },
+  {
+    type: "Securities Business and Regulations",
+    img: "/assets/images/practices/image 76.svg",
+  },
+  {
+    type: "Sovereign Debt",
+    img: "/assets/images/practices/image 77.svg",
+  },
+  {
+    type: "Sukuk and Debt Instruments",
+    img: "/assets/images/practices/image 78.svg",
+  },
+  {
+    type: "Saudi Law Expert Advice",
+    img: "/assets/images/practices/image 79.svg",
+  },
+  {
+    type: "Trade Restrictions, Sanctions and Export Controls",
+    img: "/assets/images/practices/image 81.svg",
+  },
+  {
+    type: "Trade, Commodities and Export Finance",
+    img: "/assets/images/practices/image 82.svg",
+  },
+  // {
+  //   type: "Banks",
+  //   img: "/assets/images/practices/image 80.svg",
+  // },
+  {
+    type: "Tax and Zakat Disputes",
+    img: "/assets/images/practices/image 80.svg",
+  },
+];
---
+++ data/sectors.js
@@ -0,0 +1,42 @@
+export const sectors = [
+  {
+    type: "Banks",
+    img: "/assets/images/sectors/image 83.svg",
+  },
+  {
+    type: "Education",
+    img: "/assets/images/sectors/image 84.svg",
+  },
+  {
+    type: "Energy",
+    img: "/assets/images/sectors/image 85.svg",
+  },
+  {
+    type: "Health Care",
+    img: "/assets/images/sectors/image 86.svg",
+  },
+  {
+    type: "Infrastructure, Transport & Logistics",
+    img: "/assets/images/sectors/image 87.svg",
+  },
+  {
+    type: "Oil & Gas",
+    img: "/assets/images/sectors/image 88.svg",
+  },
+  {
+    type: "Petrochemicals",
+    img: "/assets/images/sectors/image 89.svg",
+  },
+  {
+    type: "Hotels & Leisure",
+    img: "/assets/images/sectors/image 90.svg",
+  },
+  {
+    type: "Retail and Consumer",
+    img: "/assets/images/sectors/image 91.svg",
+  },
+  {
+    type: "Telecommunications, Media and Technology",
+    img: "/assets/images/sectors/image 92.svg",
+  },
+];
